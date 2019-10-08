4b54da7c8bec1dcab2ff6c3fbb64ab7bb6877ac2
flexsant <flexsant@gmail.com>
2019-10-07 21:19:34 -0700

    server.js looping the total difference to match friends, using math.abs for absolute difference

--- server.js
+++ server.js
@@ -1,6 +1,6 @@
 // dependencies
 var express = require("express");
-var path = require("path")
+var path = require("path");
 
 // set up express
 var app = express();
@@ -14,13 +14,18 @@ app.use(express.json());
 var friends = require("./app/data/friends");
 
 //html routes
-app.get("/", function (req,res) {
+app.get("/", function (req, res) {
+    res.sendFile(path.join(__dirname, "./app/public/survey.html"));
+});
+
+//tables.html
+app.get("/survey", function (req, res) {
     res.sendFile(path.join(__dirname, "./app/public/survey.html"));
 });
 
 // api/friends display all tables
 app.get("/api/friends", function (req, res) {
-    retrun res.json(friends);
+    return res.json(friends);
 });
 
 // post api/friends
@@ -37,7 +42,39 @@ app.post("/api/friends", function (req, res) {
         difference: 999
     }
 
-    
-})
+for (var i = 0; i < friends.length; i++) {
+    currentFriend = friends[i];
+    currentFriendScores = friends[i].scores;
+
+    totalDifference = 0;
+
+    for (var j = 0; j < currentFriendScores.length; j++) {
+        var difference = 0;
+        //the current score we are looping over of the friend from the json
+        currentFriendScore = currentFriendScores[j];
+        //the same index of the scores from the newFriend
+        newFriendScore = newFriendScores[j];
+        //figure out the difference
+        difference = Math.abs(parseInt(currentFriendScore) - parseInt(newFriendScore));
+        //figure out the difference and add it to the totalDifference
+        totalDifference += difference
+    }
+
+    if(totalDifference <= match.difference) {
+        match.name = currentFriend.name;
+        match.photo = currentFriend.photo;
+        match.difference = totalDifference;
+    }
+}
+
+    //push new friend to friend array, happens at the very end
+    friends.push(newFriend);
+    //send match to front end
+    res.json(match);
+});
+    //start server-listener
+    app.listen(PORT, function() {
+        console.log("App listening on PORT: " + PORT);
+    });
 
 
