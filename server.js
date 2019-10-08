60d606187f6efef0708abc0774a52a6a90c723ce
flexsant <flexsant@gmail.com>
2019-10-07 20:25:29 -0700

    routes

--- server.js
+++ server.js
@@ -4,12 +4,40 @@ var path = require("path")
 
 // set up express
 var app = express();
+
 var PORT = process.env.PORT || 3000;
 
 // handle data parsing
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 
-var friends = [];
+var friends = require("./app/data/friends");
+
+//html routes
+app.get("/", function (req,res) {
+    res.sendFile(path.join(__dirname, "./app/public/survey.html"));
+});
+
+// api/friends display all tables
+app.get("/api/friends", function (req, res) {
+    retrun res.json(friends);
+});
+
+// post api/friends
+app.post("/api/friends", function (req, res) {
+    var newFriend = req.body;
+    var newFriendScores = newFriend.scores;
+    var totalDifference;
+
+    console.log(newFriend);
+
+    var match = {
+        name: "",
+        photo: "",
+        difference: 999
+    }
+
+    
+})
 
 
