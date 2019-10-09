a2355552b4faf69e4edf10cfdb68f8f3f24d9a81
flexsant <flexsant@gmail.com>
2019-10-08 18:59:38 -0700

    survey.html

--- server.js
+++ server.js
@@ -42,39 +42,39 @@ app.post("/api/friends", function (req, res) {
         difference: 999
     }
 
-for (var i = 0; i < friends.length; i++) {
-    currentFriend = friends[i];
-    currentFriendScores = friends[i].scores;
+    for (var i = 0; i < friends.length; i++) {
+        currentFriend = friends[i];
+        currentFriendScores = friends[i].scores;
 
-    totalDifference = 0;
+        totalDifference = 0;
 
-    for (var j = 0; j < currentFriendScores.length; j++) {
-        var difference = 0;
-        //the current score we are looping over of the friend from the json
-        currentFriendScore = currentFriendScores[j];
-        //the same index of the scores from the newFriend
-        newFriendScore = newFriendScores[j];
-        //figure out the difference
-        difference = Math.abs(parseInt(currentFriendScore) - parseInt(newFriendScore));
-        //figure out the difference and add it to the totalDifference
-        totalDifference += difference
+        for (var j = 0; j < currentFriendScores.length; j++) {
+            var difference = 0;
+            //the current score we are looping over of the friend from the json
+            currentFriendScore = currentFriendScores[j];
+            //the same index of the scores from the newFriend
+            newFriendScore = newFriendScores[j];
+            //figure out the difference
+            difference = Math.abs(parseInt(currentFriendScore) - parseInt(newFriendScore));
+            //figure out the difference and add it to the totalDifference
+            totalDifference += difference
+        }
+
+        if (totalDifference <= match.difference) {
+            match.name = currentFriend.name;
+            match.photo = currentFriend.photo;
+            match.difference = totalDifference;
+        }
     }
-
-    if(totalDifference <= match.difference) {
-        match.name = currentFriend.name;
-        match.photo = currentFriend.photo;
-        match.difference = totalDifference;
-    }
-}
 
     //push new friend to friend array, happens at the very end
     friends.push(newFriend);
     //send match to front end
     res.json(match);
 });
-    //start server-listener
-    app.listen(PORT, function() {
-        console.log("App listening on PORT: " + PORT);
-    });
+//start server-listener
+app.listen(PORT, function () {
+    console.log("App listening on PORT: " + PORT);
+});
 
 
